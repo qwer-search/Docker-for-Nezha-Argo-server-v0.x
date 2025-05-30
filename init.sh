4fdb56157f27b89bd97ebd948441b7065e30c198
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2025-05-30 23:33:37 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -398,6 +398,7 @@ if command -v base64 >/dev/null 2>&1; then
 fi
 x_url="${up_url}\n${vm_url}"
 encoded_url=$(echo -e "${x_url}\n${up_url2}" | base64 -w 0)
+echo -e $encoded_url > /tmp/list.log
 echo "============  <订阅地址(隧道端口要设置80):>  ========  "
 echo "  "
 echo "$ARGO_DOMAIN/$UUID"
